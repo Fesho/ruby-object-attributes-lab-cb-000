4402f032daa27347af743440f410089948ab35d9
Mostafa Elfishawy <fisho_world@yahoo.com>
2019-01-17 18:15:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,21 @@
+
 class Dog
 
-def name=(name)
-  
-  fido.name=name
-end
+   def name=(name)
+    @name = name
+  end
 
-end
+   def name
+    @name
+  end
+
+   def breed=(breed)
+    @breed = breed
+  end
+
+   def breed
+    @breed
+  end
+
+
+ end
